8b28fc3d4c706d3764c2b8612ac25e84b65dd734
Vlad Nistor <vlad@verisart.com>
2023-08-06 11:16:09 +0100

    ready for public

--- src/main/kotlin/runner/AsyncRunner.kt
+++ src/main/kotlin/runner/AsyncRunner.kt
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory
 
 
 const val MAX_CONCURRENT_REQUESTS = 30
-const val RETRY_INTERVAL = 50L
 
 class AsyncRunner(
     private val proxies: List<InputService>,
@@ -28,41 +27,32 @@ class AsyncRunner(
     // Codes that have been successfully processed and can be written to the output file
     private val outputChannel: Channel<Pair<String, String>> = Channel(Channel.UNLIMITED)
 
-
-    private suspend fun waitUntil(f : () -> Boolean){
-        while(!f()){
-            delay(RETRY_INTERVAL)
-        }
-    }
-
     private suspend fun runProxy(proxy: InputService) = coroutineScope{
-        val procs = AtomicInteger(0)
-
-        for(code in inputChannel){
-            waitUntil{ procs.get() < MAX_CONCURRENT_REQUESTS }
-            procs.incrementAndGet()
+        repeat(MAX_CONCURRENT_REQUESTS){
             launch {
-                callAndProcess(proxy, code)
-                procs.decrementAndGet()
+                doWork(proxy)
             }
         }
     }
 
-    private suspend fun callAndProcess(proxy: InputService, code : String) {
-        val response = proxy.call(code)
-        when(response.code()){
-            200 -> {
-                logger.debug("Got response for $code : ${response.body()!!.information}")
-                outputChannel.send(Pair(code, response.body()!!.information))
-            }
-            503 -> {
-                logger.error("Got 503 for $code, retrying")
-                inputChannel.send(code)
-            }
-            else -> {
-                throw IllegalStateException("Got unexpected response code ${response.code()} for $code")
+    private suspend fun doWork(proxy: InputService) {
+        for(code in inputChannel){
+            val response = proxy.call(code)
+            when(response.code()){
+                200 -> {
+                    logger.debug("Got response for $code : ${response.body()!!.information}")
+                    outputChannel.send(Pair(code, response.body()!!.information))
+                }
+                503 -> {
+                    logger.error("Got 503 for $code, retrying")
+                    inputChannel.send(code)
+                }
+                else -> {
+                    throw IllegalStateException("Got unexpected response code ${response.code()} for $code")
+                }
             }
         }
+
     }
 
     private suspend fun outputToFile(){
